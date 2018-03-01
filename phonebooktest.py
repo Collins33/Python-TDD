55c6bd642956b703be7e111cabc37f2a9990352e
Collins njau <collinsnjau39@gmail.com>
2018-03-01 10:06:54 +0300

    Add the setup test that will run before each test

--- phonebooktest.py
+++ phonebooktest.py
@@ -6,7 +6,12 @@ class TestPhonebook(unittest.TestCase):
 
     #test if it is an instance of phonebook object
 
-    def test_initialize_phonebook(self):
+    def setUp(self):
+        #set up method runs before each test
+        #it creates instance of Contact class
         self.new_contact=Contact("collins","njau","0702848032")
 
+
+    def test_initialize_phonebook(self):
+        
         self.assertEqual(self.new_contact.first_name, "collins")
